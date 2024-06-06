78451008add31402264573f9428123ea8a04e1e1
Lovette Duke <qutyluv@gmail.com>
2024-06-06 17:11:09 +0100

    Add files via upload
    Mvp-0

---
+++ Home.py
@@ -0,0 +1,28 @@
+import streamlit as sl
+
+sl.set_page_config(
+    page_title='My Portfolio'
+)
+
+sl.sidebar.success('MENU')
+sl.title('D4D')
+sl.header('Dashboard For Data Driven Decisions')
+sl.image("luke-chesser-JKUTrJ4vK00-unsplash.jpg", use_column_width=True)
+
+sl.write("""This project was inspired by my facination for the Data 
+         and AI field.""")
+
+sl.image('feature.png')
+sl.write(""" This is the sidebar for the visualization app created
+         with streamlit displaying a Histogram with a dropdown to select
+         the feature to visualize and the number of bins for the plot""")
+
+sl.markdown("**Connect With Me**")
+
+sl.markdown("[LinkedIn](https://www.linkedin.com/in/lovetteduke)")
+
+sl.markdown("[X-Twitter](https://x.com/lovetteDuke)")
+
+sl.markdown("[Github](https://github.com/Lovette-Duke)")
+
+sl.write("© 2024 Lovette Duke All rights reserved.")
